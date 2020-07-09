2d7653d5a7a664f67fc11c549554b73d0a1c9e54
edgar-orange <EDGAR_AGUIRRE@homedepot.com>
2020-07-08 22:32:15 -0500

    done

--- fuelProblem.js
+++ fuelProblem.js
@@ -1,5 +1,16 @@
-function calcualteFuel(mass) {
-  return Math.floor(mass/3) - 2 ;
+let arrayCounter = [12, 14, 1969, 100756];
+
+function calcualteFuel(arrayCounter) {
+  let totalFuelReq = 0;
+
+  for(let i = 0; i < arrayCounter.length; i++) {
+    totalFuelReq = totalFuelReq + Math.floor(arrayCounter[i]/3) - 2;
+  }
+
+  // console.log(totalFuelReq);
+  return totalFuelReq;
 };
+
+console.log(calcualteFuel(arrayCounter));
 
 module.exports = calcualteFuel; 
